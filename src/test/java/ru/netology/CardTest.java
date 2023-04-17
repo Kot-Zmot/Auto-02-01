06972aeb8449d8d83100b5cec70ecd86a00b9fc0
Kot-Zmot <meledin.av52@gmail.com>
2023-04-17 18:25:14 +0300

    mentor`s questions

--- src/test/java/ru/netology/CardTest.java
+++ src/test/java/ru/netology/CardTest.java
@@ -13,27 +13,10 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class CardTest {
-//    private WebDriver driver;
-//
-//    @BeforeAll
-//    static void setUpAll() {
-//        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
-//    }
-//
-//    @BeforeEach
-//    void setUp() {
-//        driver = new ChromeDriver();
-//    }
-//
-//    @AfterEach
-//    void tearDown() {
-//        driver.quit();
-//        driver = null;    }
 
     @Test
     void webFormSuccessTest() {
         open("http://localhost:9999");
-//        SelenideElement form = $("[data-test-id=cardtest-form]");
         $("[data-test-id=name] input").setValue("Киркорова Алла-Виктория");
         $("[data-test-id=phone] input").setValue("+79998887766");
         $("[data-test-id=agreement]").click();
